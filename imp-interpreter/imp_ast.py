697f8eb9b5be6c7f04a51d5800caa23aab259afa
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-05-25 15:40:09 +0100

    Now WhileStatement has an invariant by default

--- imp-interpreter/imp_ast.py
+++ imp-interpreter/imp_ast.py
@@ -34,88 +34,6 @@ class Aexp(Equality):
 class Bexp(Equality):
     pass
 
-class AssignStatement(Statement):
-    def __init__(self, name, aexp):
-        self.name = name
-        self.aexp = aexp
-
-    def __repr__(self):
-        return 'AssignStatement(%s, %s)' % (self.name, self.aexp)
-
-    def eval(self, env):
-        value = self.aexp.eval(env)
-        env[self.name] = value
-
-class CompoundStatement(Statement):
-    def __init__(self, first, second):
-        self.first = first
-        self.second = second
-
-    def __repr__(self):
-        return 'CompoundStatement(%s, %s)' % (self.first, self.second)
-
-    def eval(self, env):
-        self.first.eval(env)
-        self.second.eval(env)
-
-class IfStatement(Statement):
-    def __init__(self, condition, true_stmt, false_stmt):
-        self.condition = condition
-        self.true_stmt = true_stmt
-        self.false_stmt = false_stmt
-
-    def __repr__(self):
-        return 'IfStatement(%s, %s, %s)' % (self.condition, self.true_stmt, self.false_stmt)
-
-    def eval(self, env):
-        condition_value = self.condition.eval(env)
-        if condition_value:
-            self.true_stmt.eval(env)
-        else:
-            if self.false_stmt:
-                self.false_stmt.eval(env)
-
-class WhileStatement(Statement):
-    def __init__(self, condition, body):
-        self.condition = condition
-        self.body = body
-
-    def __repr__(self):
-        return 'WhileStatement(%s, %s)' % (self.condition, self.body)
-
-    def eval(self, env):
-        condition_value = self.condition.eval(env)
-        while condition_value:
-            self.body.eval(env)
-            condition_value = self.condition.eval(env)
-
-class VCStatement(Statement):
-    def __init__(self, condition):
-        self.condition = condition
-
-    def eval(self, env):
-        return
-
-class PreStatement(VCStatement):
-    def __repr__(self):
-        return 'PreStatement(%s)' % (self.condition)
-
-class PosStatement(VCStatement):
-    def __repr__(self):
-        return 'PosStatement(%s)' % (self.condition)
-
-class InvStatement(VCStatement):
-    def __repr__(self):
-        return 'InvStatement(%s)' % (self.condition)
-
-class AssumeStatement(VCStatement):
-    def __repr__(self):
-        return 'AssumeStatement(%s)' % (self.condition)
-
-class AssertStatement(VCStatement):
-    def __repr__(self):
-        return 'AssertStatement(%s)' % (self.condition)
-
 class IntAexp(Aexp):
     def __init__(self, i):
         self.i = i
@@ -227,3 +145,86 @@ class NotBexp(Bexp):
     def eval(self, env):
         value = self.exp.eval(env)
         return not value
+
+class AssignStatement(Statement):
+    def __init__(self, name, aexp):
+        self.name = name
+        self.aexp = aexp
+
+    def __repr__(self):
+        return 'AssignStatement(%s, %s)' % (self.name, self.aexp)
+
+    def eval(self, env):
+        value = self.aexp.eval(env)
+        env[self.name] = value
+
+class CompoundStatement(Statement):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def __repr__(self):
+        return 'CompoundStatement(%s, %s)' % (self.first, self.second)
+
+    def eval(self, env):
+        self.first.eval(env)
+        self.second.eval(env)
+
+class IfStatement(Statement):
+    def __init__(self, condition, true_stmt, false_stmt):
+        self.condition = condition
+        self.true_stmt = true_stmt
+        self.false_stmt = false_stmt
+
+    def __repr__(self):
+        return 'IfStatement(%s, %s, %s)' % (self.condition, self.true_stmt, self.false_stmt)
+
+    def eval(self, env):
+        condition_value = self.condition.eval(env)
+        if condition_value:
+            self.true_stmt.eval(env)
+        else:
+            if self.false_stmt:
+                self.false_stmt.eval(env)
+
+class WhileStatement(Statement):
+    def __init__(self, condition, body, invariant=RelopBexp('=', IntAexp(0), IntAexp(0))): # TODO change this to true
+        self.condition = condition
+        self.body = body
+        self.invariant = invariant
+
+    def __repr__(self):
+        return 'WhileStatement(%s, %s, %s)' % (self.condition, self.body, self.invariant)
+
+    def eval(self, env):
+        condition_value = self.condition.eval(env)
+        while condition_value:
+            self.body.eval(env)
+            condition_value = self.condition.eval(env)
+
+class VCStatement(Statement):
+    def __init__(self, condition):
+        self.condition = condition
+
+    def eval(self, env):
+        return
+
+class PreStatement(VCStatement):
+    def __repr__(self):
+        return 'PreStatement(%s)' % (self.condition)
+
+class PosStatement(VCStatement):
+    def __repr__(self):
+        return 'PosStatement(%s)' % (self.condition)
+
+class InvStatement(VCStatement):
+    def __repr__(self):
+        return 'InvStatement(%s)' % (self.condition)
+
+class AssumeStatement(VCStatement):
+    def __repr__(self):
+        return 'AssumeStatement(%s)' % (self.condition)
+
+class AssertStatement(VCStatement):
+    def __repr__(self):
+        return 'AssertStatement(%s)' % (self.condition)
