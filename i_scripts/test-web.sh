70c8d384fa12660d18d03189dad0d2c164fc00ec
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-06-02 22:49:50 +0100

    add webserver

--- i_scripts/test-web.sh
+++ i_scripts/test-web.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST "http://localhost:8080/api/v1/igen" -d '{"code":"pre x > 100 end;while x < 1000 do inv 100 < x and x <= 1000 end;x := x + 1 end;pos x > 1000 end"}' -H "Content-Type: application/json"
+curl -X POST "http://localhost:8000/api/v1/igen" -d '{"code":"pre x > 100 end;while x < 1000 do inv 100 < x and x <= 1000 end;x := x + 1 end;pos x > 1000 end"}' -H "Content-Type: application/json"
--- webserver.py
+++ webserver.py
@@ -1,22 +1,35 @@
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-from SocketServer import ThreadingMixIn
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 from i_gen import run_vc_gen
-import threading
+import BaseHTTPServer
 import re
-import argparse
 import simplejson
- 
-class HTTPRequestHandler(BaseHTTPRequestHandler):
- 
+import json
+
+class Reply:
+    def __init__(self, vcs, result):
+        self.vcs = vcs
+        self.result = result
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
     def do_POST(self):
         if None != re.search('/api/v1/igen', self.path):
             if self.headers['Content-Type'] == 'application/json':
-                self.json_string = self.rfile.read(int(self.headers['Content-Length']))
-                json = simplejson.loads(self.json_string)
-                code = json['code']
+                json_string = self.rfile.read(int(self.headers['Content-Length']))
+                json_decoded = simplejson.loads(json_string)
+                code = json_decoded['code']
+
                 (vcs, output) = run_vc_gen(code)
-                print vcs
-                print output
+                reply = Reply(vcs, output)
+                json_reply = json.dumps(reply.__dict__)
+
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json_reply)
             else:
                 self.send_response(200)
                 self.end_headers()
@@ -25,37 +38,6 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
         return
- 
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    allow_reuse_address = True
- 
-    def shutdown(self):
-        self.socket.close()
-        HTTPServer.shutdown(self)
- 
-class SimpleHttpServer():
-    def __init__(self, ip, port):
-        self.server = ThreadedHTTPServer((ip,port), HTTPRequestHandler)
 
-    def start(self):
-        self.server_thread = threading.Thread(target=self.server.serve_forever)
-        self.server_thread.daemon = True
-        self.server_thread.start()
-
-    def waitForThread(self):
-        self.server_thread.join()
-
-    def stop(self):
-        self.server.shutdown()
-        self.waitForThread()
- 
-if __name__=='__main__':
-    parser = argparse.ArgumentParser(description='HTTP Server')
-    parser.add_argument('port', type=int, help='Listening port for HTTP Server')
-    parser.add_argument('ip', help='HTTP Server IP')
-    args = parser.parse_args()
-
-    server = SimpleHttpServer(args.ip, args.port)
-    print 'HTTP Server Running...........'
-    server.start()
-    server.waitForThread()
+if __name__ == '__main__':
+    BaseHTTPServer.test(CORSRequestHandler, BaseHTTPServer.HTTPServer)
